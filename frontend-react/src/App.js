54327262f0983702a696d19eed5483893195da78
Nadim <nahlenadim@gmail.com>
2022-10-14 13:40:07 +0300

    Configure React Router

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,9 +1,23 @@
 
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 function App() {
+
   return (
-    <div className="App">
-      Welcome
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/*PUBLIC ROUTES*/}
+
+          {/*PROTECTED ROUTES*/}
+
+
+
+          {/*404 ROUTE*/}
+
+        </Route>
+      </Routes>
     </div>
   );
 }
