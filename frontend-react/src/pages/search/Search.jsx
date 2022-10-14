3045651c7e6903a09d7d35ea8372591dee0656b3
Nadim <nahlenadim@gmail.com>
2022-10-14 18:50:28 +0300

    Create Search State

--- frontend-react/src/pages/search/Search.jsx
+++ frontend-react/src/pages/search/Search.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 const Search = () => {
+  const [serachInput, setSearchInput] = useState("");
   return (
     <div className="search">
       <div className="search-center">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Search for an artist          &#xf007;"
-        />
+        <form>
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search for an artist"
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                console.log("good");
+              }
+            }}
+            onChange={(e) => setSearchInput(e.currentTarget.value)}
+          />
+        </form>
       </div>
     </div>
   );
