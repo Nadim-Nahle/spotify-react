5f8592bc5770e2f2d87d78da540c0bbd56e984e1
Nadim <nahlenadim@gmail.com>
2022-10-14 17:50:16 +0300

    Create Search Bar

--- frontend-react/src/pages/search/Search.jsx
+++ frontend-react/src/pages/search/Search.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Search.css";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="search-center">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search for an artist          &#xf007;"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
