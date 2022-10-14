a3036eb6708c60ff0f6e68dd935381eb32ebaa2e
Nadim <nahlenadim@gmail.com>
2022-10-14 16:50:41 +0300

    Create Login Button

--- frontend-react/src/components/login/Login.jsx
+++ frontend-react/src/components/login/Login.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Login.css";
 
+const CLIENT_ID = "105a826077134eff89ba8d64e0480807";
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL = "http://localhost:300/callback";
+
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <button className="login-btn">Login</button>
+    </div>
+  );
 };
 
 export default Login;
--- frontend-react/src/pages/callback/CallBack.jsx
+++ frontend-react/src/pages/callback/CallBack.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-const CLIENT_ID = "105a826077134eff89ba8d64e0480807";
-const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL = "http://localhost:300/callback";
-
 const CallBack = () => {
   return <div>CallBack</div>;
 };
