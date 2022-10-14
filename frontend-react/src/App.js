62847644862b4a47c4afb6fd7465e3921030595c
Nadim <nahlenadim@gmail.com>
2022-10-14 14:05:41 +0300

    Create Login Router

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import Login from "./components/login/Login";
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           {/*PUBLIC ROUTES*/}
-
+          <Route path="/" element={<Login />}></Route>
           {/*PROTECTED ROUTES*/}
 
 
